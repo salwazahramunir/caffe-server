673c37b4b8f75faca0e361a683b9a970e3d5df62
salwazahramunir <salwazahramunir@gmail.com>
2022-08-21 08:19:11 +0700

    change response menu controller

--- caffe-server/controller/MenuController.js
+++ caffe-server/controller/MenuController.js
@@ -2,7 +2,15 @@ const { Menu, DrinkConcoction, RawMaterial, KitchenStore } = require('../models/
 class MenuController {
     static async readAllMenu(req, res, next) {
         try {
-            const menus = await Menu.findAll({ include: DrinkConcoction })
+            const menus = await Menu.findAll({
+                include: [
+                    {
+                        model: DrinkConcoction,
+                        attributes: { exclude: ["createdAt", "updatedAt"]}
+                    }
+                ],
+                attributes: { exclude: ["createdAt", "updatedAt"]}
+            })
 
             res.status(200).json({
                 message: "Success read menus",
@@ -17,6 +25,24 @@ class MenuController {
         try {
             const { name, price, category, isConcoction, isAvaiable } = req.body
 
+            if (+category === 0) {
+                res.status(400).json({
+                    message: "Category is required"
+                })
+            }
+
+            if (+isConcoction === 0 && category === "Drink") {
+                res.status(400).json({
+                    message: "Concoction is required"
+                })
+            }
+
+            if (+isAvaiable === 0) {
+                res.status(400).json({
+                    message: "Avaiable is required"
+                })
+            }
+
             const newMenu = await Menu.create({ name, price, category, isConcoction, isAvaiable })
 
             if (category === "Drink" && isConcoction === true || isConcoction === "true" ) {
@@ -28,8 +54,7 @@ class MenuController {
             }
 
             res.status(201).json({
-                message: "Success create menu",
-                menu: newMenu
+                message: `Success create ${newMenu.name}`,
             })
         } catch (error) {
             next(error)
@@ -40,10 +65,25 @@ class MenuController {
         try {
             const { id } = req.params
 
-            const findMenu = await Menu.findByPk(+id, { include: [{
-                    model: DrinkConcoction,
-                    include: [{ model: RawMaterial, include: KitchenStore }]
-                }]})
+            const findMenu = await Menu.findByPk(+id, {
+                include: [
+                    {
+                        model: DrinkConcoction,
+                        include: [
+                            {
+                                model: RawMaterial,
+                                include: [{
+                                    model: KitchenStore,
+                                    attributes: { exclude: ["createdAt", "updatedAt"]}
+                                }],
+                                attributes: { exclude: ["createdAt", "updatedAt"]}
+                            }
+                        ],
+                        attributes: { exclude: ["createdAt", "updatedAt"]}
+                    }
+                ],
+                attributes: { exclude: ["createdAt", "updatedAt"]}
+            })
 
             if (!findMenu) {
                 throw { name: "NotFound" }
@@ -70,19 +110,60 @@ class MenuController {
             }
 
             await Menu.update({ name, price, category, isConcoction, isAvaiable }, { where: { id }})
-            findMenu = await Menu.findByPk(+id)
+            findMenu = await Menu.findByPk(+id, {
+                include: [
+                    {
+                        model: DrinkConcoction,
+                        attributes: { exclude: ["createdAt", "updatedAt"]}
+                    }
+                ],
+                attributes: { exclude: ["createdAt", "updatedAt"]}
+            })
 
             if (category === "Drink" && isConcoction === true || isConcoction === "true") {
                 const { recipes } = req.body
+                let recipesDelete = []
+
+                for (let i = 0; i < findMenu.dataValues.DrinkConcoctions.length; i++) {
+                    const drinkConcoction = findMenu.dataValues.DrinkConcoctions[i];
+                    let flag = false
+                    
+                    for (let j = 0; j < recipes.length; j++) {
+                        const recipe = recipes[j];
+                        
+                        if (drinkConcoction.id === recipe.id) {
+                            flag = true
+                            break;
+                        } else {
+                            flag = false
+                        }
+                    }
+
+                    if (!flag) {
+                        recipesDelete.push(drinkConcoction)
+                    }
+                }
+
+                recipesDelete.forEach(el => {
+                    const { id } = el
+
+                    DrinkConcoction.destroy({ where: { id } })
+                })
+
+                
                 recipes.forEach(el => {
                     const { id, raw_material_id, dose } = el
-                    DrinkConcoction.update({ menu_id: findMenu.id, raw_material_id, dose }, { where: { id } })
+
+                    if (!id) {
+                        DrinkConcoction.create({ menu_id: findMenu.id, raw_material_id, dose })
+                    } else {
+                        DrinkConcoction.update({ menu_id: findMenu.id, raw_material_id, dose }, { where: { id } })
+                    }
                 });
             }
 
             res.status(201).json({
-                message: "Success update menu",
-                menu: findMenu
+                message: `Success update ${findMenu.name}`,
             })
         } catch (error) {
             next(error)
